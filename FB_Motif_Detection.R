a391fb43eabfd70af8c6d9c717e1b1de73237ab2
dongbusun <37465141+dongbusun@users.noreply.github.com>
2019-03-11 15:30:56 +0800

    Add files via upload

---
+++ FB_Motif_Detection.R
@@ -0,0 +1,271 @@
+
+## Detect feedback loops in the GRN of glioma drug resistance 
+
+
+setwd("F:/胶质瘤诱导分化组学数据分析/Code-cluster_based/Results") 
+
+
+############# Read sensitive network coefficient
+B_S=read.table("Significant Coefficient_Sensitive Network.xls",header = F,sep="\t",fileEncoding = "UTF-8")
+B_Net=B_S
+rownames(B_Net)=B_Net[,1]
+# colnames(B_Net)
+B_Net=B_Net[,-1]   #53X53
+
+# ############# Read resistant network coefficient
+# A=read.table("Coefficient_Resistant Network.xls",header = T,sep="\t",fileEncoding = "UTF-8",row.names=NULL)
+# 
+# B=A[,-c(1,2)]
+# B=as.matrix(B)
+# dim(B)
+# B[abs(B)<=0.01]=0
+# sum(sum(B!=0))   # 100
+# 
+# rn=rownames(edge_R[rowSums(abs(edge_R))!=0, ])
+# cn=colnames(edge_R[,colSums(abs(edge_R))!=0])
+# Net_Gene=union(rn,cn)
+# 
+# rownames(B)=Net_Gene
+# colnames(B)=Net_Gene
+# 
+# 
+# 
+# # B=read.table("Significant Coefficient_Resistant Network.xls",header = T,sep="\t")
+# edge_R=read.table("edge matrix for Resistant genes.txt",header = T,sep="\t")
+# rownames(edge_R)=edge_R[,1]
+# edge_R=edge_R[,-1]
+# 
+# ### Select genes in the network
+# a=rownames(edge_R[rowSums(abs(B))!=0, ])
+# b=colnames(edge_R[,colSums(abs(B))!=0])
+# DirectedNet_Gene=union(a,b)
+# 
+# B_Net=B[DirectedNet_Gene,DirectedNet_Gene]
+# dim(B_Net)  # 44,44
+# 
+# write.table(B_Net, file="Coefficient_GeneNetwork_Resistant.txt",col.names = NA,sep = "\t")
+
+############
+B_R=read.table("Significant Coefficient_Resistant Network.xls",header = F,sep="\t",fileEncoding = "UTF-8")
+B_Net=B_R
+rownames(B_Net)=B_Net[,1]
+# colnames(B_Net)
+B_Net=B_Net[,-1]   #44X44
+##############
+
+
+###########  FB detection ################
+N=dim(B_Net)[1]
+NFB=NULL
+
+for (i in 1:N)
+{
+  for (j in i+1:N)
+  {
+    if (j<=N)
+    {
+      print(i)
+      print(j)
+      if ((B_Net[i,j]<0 & B_Net[j,i]>0) || (B_Net[i,j]>0 & B_Net[j,i]<0))
+      {
+        NFB=rbind(NFB,cbind(rownames(B_Net)[i],rownames(B_Net)[j],B_Net[i,j],B_Net[j,i]))
+      }
+    }
+    
+  }
+}
+
+NFB
+
+
+N=dim(B_Net)[1]
+
+PFB=NULL
+
+for (i in 1:N)
+{
+  for (j in i+1:N)
+  {
+    if (j<=N)
+    {
+      print(i)
+      print(j)
+      if ((B_Net[i,j]>0 & B_Net[j,i]>0))
+      {
+        PFB=rbind(PFB,cbind(rownames(B_Net)[i],rownames(B_Net)[j],B_Net[i,j],B_Net[j,i]))
+      }
+    }
+    
+  }
+}
+
+PFB
+
+
+N=dim(B_Net)[1]
+
+PFB=NULL
+
+for (i in 1:N)
+{
+  for (j in i+1:N)
+  {
+    if (j<=N)
+    {
+      print(i)
+      print(j)
+      if ((B_Net[i,j]>0 & B_Net[j,i]>0) || (B_Net[i,j]<0 & B_Net[j,i]<0))
+      {
+        PFB=rbind(PFB,cbind(rownames(B_Net)[i],rownames(B_Net)[j],B_Net[i,j],B_Net[j,i]))
+      }
+    }
+    
+  }
+}
+
+PFB
+
+
+
+PNFB=NULL  # coupled FB
+M1=dim(PFB)[1]
+M2=dim(NFB)[1]
+for (k in 1:M1)
+{
+  for (m in 1:M2)
+  {
+    if (k<=M1 & m<=M2)
+    {
+      print(k)
+      print(m)
+      if (length(intersect(PFB[k,1:2],NFB[m,1:2]))!=0)
+        PNFB=rbind(PNFB,c(PFB[k,],NFB[m,]))
+    }
+    
+  }
+}
+
+PNFB
+
+PPFB=NULL  # coupled FB
+M1=dim(PFB)[1]
+M2=dim(PFB)[1]
+for (k in 1:M1)
+{
+  for (m in 1:M2)
+  {
+    if (k<=M1 & m<=M2)
+    {
+      print(k)
+      print(m)
+      if (length(intersect(PFB[k,1:2],PFB[m,1:2]))!=0)
+        PPFB=rbind(PPFB,c(PFB[k,],PFB[m,]))
+    }
+    
+  }
+}
+
+NNFB
+
+
+NNFB=NULL  # coupled FB
+M1=dim(NFB)[1]
+M2=dim(NFB)[1]
+for (k in 1:M1)
+{
+  for (m in 1:M2)
+  {
+    if (k<=M1 & m<=M2)
+    {
+      print(k)
+      print(m)
+      if (length(intersect(NFB[k,1:2],NFB[m,1:2]))!=0)
+        NNFB=rbind(NNFB,c(NFB[k,],NFB[m,]))
+    }
+    
+  }
+}
+
+NNFB
+
+
+n1=dim(PFB)[1]
+n2=dim(NFB)[1]
+n3=dim(PNFB)[1]
+n4=dim(PPFB)[1]
+n5=dim(NNFB)[1]
+
+
+
+Number=c(n1,n2,n3,n4,n5)
+Number
+
+TotalEdge=sum(B_Net!=0)
+
+Percent=c(n1/TotalEdge,n2/TotalEdge,n3/TotalEdge,n4/TotalEdge,n5/TotalEdge)
+Percent
+
+
+setwd("F:/胶质瘤诱导分化组学数据分析/Code-cluster_based/Results") 
+write.table(NFB, file="Negative Feedback_Links.txt",quote=F,row.names=F,col.names = F,sep = "\t")
+write.table(PNFB, file="Coupled_PN_Feedback_Links.txt",quote=F,row.names=F,col.names = F,sep = "\t")
+write.table(NNFB, file="Coupled_NN_Feedback_Links.txt",quote=F,row.names=F,col.names = F,sep = "\t")
+
+
+sum(B_S<0)
+sum(B_R<0)
+
+sum(B_S!=0)
+sum(B_R!=0)
+
+
+# 
+# ############ Network analysis 
+# 
+# 
+# library(igraph) # Load the igraph package
+# setwd("F:/胶质瘤诱导分化组学数据分析/Code-cluster_based/Results")  
+# 
+# 
+# #### Sensitive network analysis 
+# C=read.table("Cytoscape_Net_Sensitive_new.txt")
+# Net=as.data.frame(cbind(as.character(C[,1]),as.character(C[,3]),C[,2]))  # Input data for R analysis, data.frame
+# net=graph_from_data_frame(Net, directed = T,vertices = NULL)  # 
+# diam <- get_diameter(net, directed=T)
+# 
+# # Degree
+# deg_S_all <- degree(net, mode="all")
+# deg_S_in <- degree(net, mode="in")
+# deg_S_out <- degree(net, mode="out")
+# 
+# 
+# ## Centrality & centralization
+# CD=centr_degree(net, mode="in", normalized=T)
+# EC=eigen_centrality(net, directed=T, weights=NA)
+# CE=centr_eigen(net, directed=T, normalized=T)
+# 
+# 
+# #### Resistant network analysis 
+# C=read.table("Cytoscape_Net_Resistant_new.txt")
+# Net=as.data.frame(cbind(as.character(C[,1]),as.character(C[,3]),C[,2]))  # Input data for R analysis, data.frame
+# net=graph_from_data_frame(Net, directed = T,vertices = NULL)  # 
+# diam <- get_diameter(net, directed=T)
+# 
+# # Degree
+# deg_R_all <- degree(net, mode="all")
+# deg_R_in <- degree(net, mode="in")
+# deg_R_out <- degree(net, mode="out")
+# ## Centrality & centralization
+# CD=centr_degree(net, mode="in", normalized=T)
+# EC=eigen_centrality(net, directed=T, weights=NA)
+# CE=centr_eigen(net, directed=T, normalized=T)
+# 
+# install.packages('ggplot2')
+# library(ggplot2)
+# h=ggplot(as.data.frame(cbind(t(deg_R_all),t(deg_R_all))),aes(x=degree(),y=Frequency,fill=cut))+
+#   geom_bar(stat="identity",position="dodge")
+# 
+# D=(cbind((deg_S_all),(deg_R_all)))
+# write.table(D, file="Degree_S_R.txt",quote=F,row.names=F,col.names = F,sep = "\t")
+# 
+# wilcox.test(deg_S_all,deg_R_all, alternative="less", paired=F) 
---
+++ scSimulation.R
@@ -0,0 +1,143 @@
+
+# Single-cell simulation and data analysis
+
+
+
+DNG=read.csv("F:/Files from office/胶质瘤诱导分化组学数据分析/Code-cluster_based/Survival analysis/Data/Quantifing each node.csv")  # Differential network genes
+Score=as.matrix(DNG[,c(1,9)])  # Importance Score
+Nodes=Score[,1]
+setwd("F:/Files from office/胶质瘤诱导分化组学数据分析/Code-cluster_based/Results")  
+
+E_D=read.table("F:/Files from office/胶质瘤诱导分化组学数据分析/Code-cluster_based/Results/Differential Network Coefficient.txt")
+E_D=as.matrix(E_D)
+BB=sign(E_D[Nodes,Nodes])
+
+# install.packages("pracma")  # Manually install
+library('pracma')
+
+num=10000
+h=0.1
+time=seq(0,48,h)
+sc_GeneExp=matrix(0,dim(BB)[1],num)
+for (ii in 1:num)
+{
+  x=matrix(0,dim(BB)[1],length(time)+1)
+  # CC=randn(42,42)*BB*0.01
+  CC=E_D[Nodes,Nodes]*(1+randn(42,42)*0.001)
+  C=as.vector(rand(42,1)*(-0.01))
+  noise=as.vector(randn(42,1)*0.01)
+  # f <- function(t,xx) 
+  # {
+  #   dx = (CC%*%xx)+C*xx+noise  # *(1-xx/10)*xx
+  #   return(dx)
+  # }
+  # a=0
+  # b=48
+  # y0=as.vector(rand(1,dim(BB)[1]))
+  # n=100
+  # sol <- rk4sys(f, a, b, y0, n)
+  # sc_GeneExp[,ii]=sol$y[n+1,]
+  ind=1
+  x[,1]=as.vector(rand(1,dim(BB)[1]))
+  
+  for (t in time)
+  {
+    x[,ind+1]=x[,ind]+h*(CC%*%x[,ind]+C*x[,ind])+noise*sqrt(h)
+    x[x<0]=0
+    x[x>100]=100
+    ind=ind+1
+  }
+  sc_GeneExp[,ii]=x[,ind]
+  
+  
+  
+}
+
+# sol$y[n,]
+# sol$y[n-1,]
+sc_GeneExp[,ii]
+sc_GeneExp[,ii-1]
+hist(sc_GeneExp[10,])
+
+Gene=sc_GeneExp
+###########################
+
+### DM
+
+# # source("http://bioconductor.org/biocLite.R")
+# 
+# # biocLite("destiny")
+# # biocLite("rgl")
+# # install.packages("stringi")
+# 
+# library(stringi)
+# library(destiny)
+# library(rgl)
+# library(Biobase)
+
+# dm=DiffusionMap(t(Gene),n_eigs = 2)
+# dm=eigenvectors(dm)
+# dev.new()
+# plot(dm,col = "#00FF66FF", type = "p", pch = 16,cex=0.5)
+
+dm=DiffusionMap(t(Gene))
+dpt <- DPT(dm)
+dev.new()
+plot(dpt, col_by = 'branch')
+
+dev.new()
+plot(dpt, root = 3, paths_to = c(1,2), col_by = 'branch')
+
+
+dev.new()
+plot(dpt, col_by = 'branch', divide = 2, dcs = c(-1,-3,2), pch = 20)
+
+
+#####################
+#########tSNE
+# install.packages("Rtsne")
+library(Rtsne)
+
+# whole genome data
+Gene=sc_GeneExp
+
+tsne = Rtsne(t(Gene), dims = 2, perplexity=220, theta=0,verbose=TRUE, max_iter = 50)
+
+dev.new()
+plot(tsne$Y, col = "#00FF66FF", type = "p", pch = 16,cex=1) #col = "#00FF66FF",
+
+####  Heatmap
+
+# install.packages("gplots") #下载gplots程序包
+library(gtools) #加载gplots程序
+library(gplots) #加载gplots程序
+
+x=sc_GeneExp
+rownames(x)=Nodes
+
+x_Normalized=(x-apply(x,1,min))/(apply(x,1,max)-apply(x,1,min))
+x=x_Normalized
+
+dev.new()
+heatmap.2(x,col=cm.colors(255),distfun =function(x) dist(x,method ='euclidean'),hclustfun =function(x) hclust(x,method ='ward'), dendrogram = "row", scale="row", Rowv=T,Colv=F,key=T,keysize=1.5,trace="none",cexCol=0.5,cexRow=0.35,na.color=par("bg"))
+
+# dev.new()
+# heatmap.2(t(x),col=cm.colors(255),distfun =function(x) 1-cor(t(x)),hclustfun =function(x) hclust(t(x),method ='ward'), dendrogram = "row", scale="row", Rowv=T,Colv=F,key=T,keysize=1.5,trace="none",cexCol=0.5,cexRow=0.5,na.color=par("bg"))
+# 
+# 
+dev.new()
+heatmap.2(x,col=bluered(255),distfun = dist,hclustfun =function(x) hclust(x,method ='ward'),scale="row", Rowv=T,Colv=NA,dendrogram="row",key=T,keysize=1.5,trace="none",cexCol=0.5,cexRow=0.45,na.color=par("bg"))
+
+# 
+# hv1=heatmap.2(t(x),col=redblue(100),scale="none",Rowv=NA,Colv=T,dendrogram="col",key=T,keysize=1.5,trace="none",cexCol=0.5,cexRow=1)
+# hv2=heatmap.2(x,distfun = dist,hclustfun = hclust,scale="row",na.rm=TRUE,col=redblue(100),Rowv=NA,Colv=T,dendrogram="col",key=T,keysize=1.5,trace="none",cexCol=0.5,cexRow=1)
+# 
+# dev.off
+
+library(pheatmap)
+dev.off()
+
+dev.new()
+pheatmap(x,cluster_row=T, cluster_cols=T, clustering_distance_rows='euclidean',clustering_method = "ward", cellwidth = 0.35, cellheight = 5,color = colorRampPalette(c("CornflowerBlue", "white", "firebrick3"))(50), fontsize=9, fontsize_row=6,labRow=NA) #自定义颜色
+
+
